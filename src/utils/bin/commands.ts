906fb9e230355de585e86ec08194de13191f6475
Malaquias Marconi <m.marconi824@gmail.com>
2023-03-09 10:16:55 -0600

    fixes done

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -41,6 +41,15 @@ More about me:
 
 };
 
+// Donate
+export const donate = async (args: string[]): Promise<string> => {
+  return `thank you for your interest. 
+here are the ways you can support my work:
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
+`;
+};
+
 // Contact
 export const email = async (args: string[]): Promise<string> => {
   window.open(`mailto:${config.email}`);
